eb2b873d7f4b00e323a8ca492777e949ac070590
Wisanu Senhom <48409780+WisanuSenhom@users.noreply.github.com>
2023-09-08 16:42:11 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+async function getData() {
+    const records = await fetch('https://script.google.com/macros/s/AKfycbykotbNoAs9Qzn7ITWb4I-f2rU1c3siVQZ25TEibeFpcqhUE84UFXImpyewqp4IMzaEbQ/exec');
+    const data = await records.json();
+    // Submitted and coded by Jagadeesh Kumar with initial as S, you may send mail to my email address which is equal to jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is equal to jagadeesh-kumar@airtel .
+    let output = '';
+ 
+      data.user.forEach(function(user){
+        output += `
+        <div class="col">
+        <div class="card text-center border-0">
+        <img class="card-img-top" src="${user.pic}" alt="Card image cap">
+             <h5 class="card-header">
+            ${user.name} 
+             </h5>
+          <h7 class="card-header">
+          (${user.position})
+          </h7>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item">การปฏิบัติงาน : ${user.work}</li>
+            <li class="list-group-item">วันที่ เวลามา : ${user.datein}</li>
+            <li class="list-group-item">วันที่ เวลากลับ : ${user.outin}</li>
+            <li class="list-group-item">ระยะห่างจากสำนักงาน : ${user.distance}</li>
+            <li class="list-group-item">ชี้แจง : ${user.note}</li>
+            <div class="d-grid gap-2 d-md-block">
+            <a href="#" class="btn btn-outline-success" type="button">อนุญาต</a>
+            <a href="#" class="btn btn-outline-danger" type="button">ไม่อนุาต</a>
+            </div>
+          </ul>
+        </div>
+          <div class="card shadow-sm">Datetimestamp : ${user.dupdate}
+          </div>
+        </div>    
+        `
+      });
+      console.log(output)
+      document.getElementById('output').innerHTML = output; 
+
+}
